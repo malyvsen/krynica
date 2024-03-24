64a80ac8dbcfc5965dccd9ca752e3d5ca27e83e3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-03-24 16:44:05 +0100

    feat: add editable text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sampleText, setSampleText] = useState("Znam 25 wróżek - i co z tego?");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Krynica</h1>
+      <p>A selection of fonts I like.</p>
+      <input
+        type="text"
+        size={50}
+        value={sampleText}
+        onChange={(e) => setSampleText(e.target.value)}
+      />
+      <div className="cormorant-garamond-regular">{sampleText}</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
